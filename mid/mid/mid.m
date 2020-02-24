5951042ad45d8e299049fa73ebca5380727ec05f
Jeff Dennison <jbdenniso@gmail.com>
2020-02-24 10:36:03 -0500

    small edit
    small edit

--- mid/mid/mid.m
+++ mid/mid/mid.m
@@ -34,7 +34,7 @@ text_size = 40;
 ms=0.06;                    %duration of time post-target until responses are accepted (no keys accepted)
 cue_time = 1;
 target_time = 1;
-feedback_time = 1;
+feedback_time = 3;
 
 load([ thePath.start '/timing/run' num2str(whichrun) '.mat'])
 
@@ -55,7 +55,7 @@ catch
 end
 
 if ~isempty(practice_files)                             % checks to see whether there is practice data or not. Overwrite if there is
-    load(practice_files(end,1:end-1),'RTs'); % practice_files has an extra space character added to the string name, hence the 1:end-1 in the code
+    load(practice_files(end,1:end),'RTs'); % practice_files has an extra space character added to the string name, hence the 1:end-1 in the code
 end
 
 for i=1:length(unique(trial_cond))                     % sets starting RT_thresh for each condition
